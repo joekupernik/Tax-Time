4ad94941ea7e5ca66478c31bcd8cf2a9757e4f7a
Joe Kupernik <joekupernik@gmail.com>
2022-08-20 18:06:47 -0700

    add packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-
+import { Routes, Route } from "react-router-dom";
+import Header from "./HeaderComponent";
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Header />
+          <Route></Route>
+          <Footer />
+        </Routes>
     </div>
   );
 }
