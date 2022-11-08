09448eb8f4900e73e0f3f3cf5af371d1a6ac499a
Joe Kupernik <joekupernik@gmail.com>
2022-11-08 13:47:53 -0800

    grid changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -14,22 +14,7 @@ import ConsultingCard from './ConsultingCard';
 export default function Home () {
     return(
       <>
-          <div
-        style={{
-          backgroundImage: `url(${background})`,
-          backgroundSize:'cover',
-          height: '15rem'
-        }}>
-          <h1>Tax time</h1>
-          </div>
-          <hr
-            style={{
-              color: "black",
-              height: 3
-            }}
-          />
-          <div style={{backgroundColor: '#E9F7F9', opacity: '90%'}}>
-        <Box>
+      <Box>
         <Grid container spacing={2}>
         <Grid item xs={4}>
           <TaxCard/>
@@ -37,14 +22,13 @@ export default function Home () {
         <Grid item xs={4}>
           <ConsultingCard/>
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={4}>
           <BookkeepingCard/>
         </Grid>
       </Grid>
             
             
         </Box>
-        </div>
         </>
     );
 }
