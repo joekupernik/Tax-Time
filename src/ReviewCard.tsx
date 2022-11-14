7804bbf7eba676b0e44d1e062b70660342ca1cbe
Joe Kupernik <joekupernik@gmail.com>
2022-11-14 08:55:25 -0800

    center review content

--- src/ReviewCard.tsx
+++ src/ReviewCard.tsx
@@ -4,6 +4,7 @@ import Typography from "@mui/material/Typography";
 import Card  from "@mui/material/Card";
 import  Rating  from "@mui/material/Rating";
 import Avatar  from "@mui/material/Avatar";
+import CardContent from "@mui/material/CardContent";
 
 
 export default function ReviewCard() {
@@ -14,8 +15,10 @@ export default function ReviewCard() {
       pt: 8,
       pb: 6,
     }}>
-        <Card variant="outlined" sx={{margin: 2, padding: 2}}>
+        <Card variant="outlined" sx={{margin: 2, padding: .2}}>
+          <CardContent sx={{display: "flex", justifyContent: "center"}}>
         <Rating defaultValue={5} readOnly />
+      </CardContent>
       <Typography  
         component="body"
         align="center"
@@ -24,14 +27,18 @@ export default function ReviewCard() {
        Carly did a great job of taking care of my unique tax senario. 
        She is very communicative, and helpful.
       </Typography>
+      
       <Typography 
        component="body"
        align="center"
-       color="text.primary">
+       color="text.primary"
+       >
         - Joe 
       </Typography>
+      <CardContent sx={{display: "flex", justifyContent: "center"}}>
       <Avatar alt="Joe" src="\Static\email signiture.png"
       sx={{ width: 56, height: 56}} />
+      </CardContent>
       </Card>
       <Card variant="outlined">
         <Rating defaultValue={5} readOnly />
