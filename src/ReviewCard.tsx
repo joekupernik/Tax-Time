153bbc3097e7955a546b803b31f480f2ccc9e2d3
Joe Kupernik <joekupernik@gmail.com>
2022-11-14 09:22:13 -0800

    polish reviews css layout

--- src/ReviewCard.tsx
+++ src/ReviewCard.tsx
@@ -17,21 +17,24 @@ export default function ReviewCard() {
     }}>
         <Card variant="outlined" sx={{margin: 2, padding: .2}}>
           <CardContent sx={{display: "flex", justifyContent: "center"}}>
-        <Rating defaultValue={5} readOnly />
+        <Rating defaultValue={5} readOnly size="large" />
       </CardContent>
       <Typography  
         component="body"
         align="center"
         color="text.primary"
+        padding="2%"
         >
        Carly did a great job of taking care of my unique tax senario. 
-       She is very communicative, and helpful.
+       She is very communicative, and helpful. i nedd to see what happens when i write a rediculously long eview that just goes on and on and on
+
       </Typography>
       
       <Typography 
        component="body"
        align="center"
        color="text.primary"
+       fontSize={"1.5rem"}
        >
         - Joe 
       </Typography>
@@ -40,45 +43,61 @@ export default function ReviewCard() {
       sx={{ width: 56, height: 56}} />
       </CardContent>
       </Card>
-      <Card variant="outlined">
-        <Rating defaultValue={5} readOnly />
+      <Card variant="outlined" sx={{margin: 2, padding: .2}}>
+          <CardContent sx={{display: "flex", justifyContent: "center"}}>
+        <Rating defaultValue={5} readOnly size="large" />
+      </CardContent>
       <Typography  
         component="body"
         align="center"
         color="text.primary"
-        
+        padding="2%"
         >
        Carly did a great job of taking care of my unique tax senario. 
-       She is very communicative, and helpful.
+       She is very communicative, and helpful. i nedd to see what happens when i write a rediculously long eview that just goes on and on and on
+
       </Typography>
+      
       <Typography 
        component="body"
        align="center"
-       color="text.primary">
+       color="text.primary"
+       fontSize={"1.5rem"}
+       >
         - Joe 
       </Typography>
-      <Avatar alt="Joe" src="\Static\email signiture.png" 
-      sx={{ width: 56, height: 56 }} />
+      <CardContent sx={{display: "flex", justifyContent: "center"}}>
+      <Avatar alt="Joe" src="\Static\email signiture.png"
+      sx={{ width: 56, height: 56}} />
+      </CardContent>
       </Card>
-      <Card variant="outlined">
-        <Rating defaultValue={5} readOnly />
+      <Card variant="outlined" sx={{margin: 2, padding: .2}}>
+          <CardContent sx={{display: "flex", justifyContent: "center"}}>
+        <Rating defaultValue={5} readOnly size="large" />
+      </CardContent>
       <Typography  
         component="body"
         align="center"
         color="text.primary"
-        
+        padding="2%"
         >
        Carly did a great job of taking care of my unique tax senario. 
-       She is very communicative, and helpful.
+       She is very communicative, and helpful. i nedd to see what happens when i write a rediculously long eview that just goes on and on and on
+
       </Typography>
+      
       <Typography 
        component="body"
        align="center"
-       color="text.primary">
+       color="text.primary"
+       fontSize={"1.5rem"}
+       >
         - Joe 
       </Typography>
-      <Avatar alt="Joe" src="\Static\email signiture.png" 
-      sx={{ width: 56, height: 56 }} />
+      <CardContent sx={{display: "flex", justifyContent: "center"}}>
+      <Avatar alt="Joe" src="\Static\email signiture.png"
+      sx={{ width: 56, height: 56}} />
+      </CardContent>
       </Card>
       </Box>
   );
