ada0227acf13abdad7289981e7dba4da5aca0863
Joe Kupernik <joekupernik@gmail.com>
2022-12-01 14:03:21 -0800

    appbar format

--- src/ButtonAppBar.tsx
+++ src/ButtonAppBar.tsx
@@ -1,10 +1,7 @@
-//import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-//import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-//import IconButton from '@mui/material/IconButton';
 import {Link} from "react-router-dom";
 
 
@@ -12,12 +9,12 @@ import {Link} from "react-router-dom";
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{pl: 2}}>
-        <Toolbar>       
-          <Button color="inherit"><Link to="/">Home</Link></Button>
-          <Button color="inherit"><Link to="/about">About</Link></Button>
-          <Button color="inherit"><Link to="/services">Services</Link></Button>
-          <Button color="inherit"><Link to="/contact">Contact</Link></Button>
+      <AppBar position="static">
+        <Toolbar >       
+          <Button color="inherit" variant="contained" size="large" sx={{mr: 2, ml: 2}} ><Link to="/">Home</Link></Button>
+          <Button color="inherit" variant="contained" size="large" sx={{mr: 2, ml: 2}}><Link to="/about">About</Link></Button>
+          <Button color="inherit" variant="contained" size="large" sx={{mr: 2, ml: 2}}><Link to="/services">Services</Link></Button>
+          <Button color="inherit" variant="contained" size="large" sx={{mr: 2, ml: 2}}><Link to="/contact">Contact</Link></Button>
         </Toolbar>
       </AppBar>
     </Box>
